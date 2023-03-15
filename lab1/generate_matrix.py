9259356656b4d0c135c00c5214b9dfde053b262a
Sirtus <tslonina@student.agh.edu.pl>
2023-03-15 21:00:38 +0100

    Renamed python function

--- lab1/generate_matrix.py
+++ lab1/generate_matrix.py
@@ -3,7 +3,7 @@ import random
 
 N = 3
 
-def generate_C_code(matrix, name):
+def generate_c_code(matrix, name):
     s = 'int ' + name + '[' + str(N) + '][' + str(N) + '] = {\n'
     for row  in matrix:
         s += '    {'
@@ -16,9 +16,9 @@ def generate_C_code(matrix, name):
     print(s)
 
 A = np.random.randint(1, 10, size=(N, N))
-generate_C_code(A, "a")
+generate_c_code(A, "a")
 B = np.random.randint(1, 10, size=(N, N))
-generate_C_code(B, "b")
+generate_c_code(B, "b")
 
 print('\n\nAB =')
 C = np.matmul(A,B)
