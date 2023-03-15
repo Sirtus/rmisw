7c0b4761f8fdd3762db2ee07e618a319f9acd088
czyjtu <maciejczyjtu@gmail.com>
2023-03-15 19:57:05 +0100

    Added Matrix struct

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -5,31 +5,40 @@
 #define A_COLS 2
 #define B_COLS 5
 
-int** classic(int** A, int** B, int a_rows, int a_cols, int b_cols) {
-    int** C = calloc(a_rows, sizeof(int*));
+typedef struct Matrix {
+    int** values;
+    int rows;
+    int cols;
+} Matrix;
+
+
+Matrix classic(Matrix A, Matrix B) {
+    int** C = calloc(A.rows, sizeof(int*));
     
-    for(int i = 0; i < a_rows; ++i) C[i] = calloc(b_cols, sizeof(int));
+    for(int i = 0; i < A.rows; ++i) C[i] = calloc(B.cols, sizeof(int));
 
     int sum = 0;
-    for(int i = 0; i < a_rows; ++i) {
-        for(int j = 0; j < b_cols; ++j) {
-            for(int k = 0; k < a_cols; ++k) {
-                sum += A[i][k] * B[k][j];
+    for(int i = 0; i < A.rows; ++i) {
+        for(int j = 0; j < B.cols; ++j) {
+            for(int k = 0; k < A.cols; ++k) {
+                sum += A.values[i][k] * B.values[k][j];
             }
             C[i][j] = sum;
             sum = 0;
         }
     }
     
-    return C;
+    Matrix M = {.values = C, .rows = A.rows, .cols=B.cols};
+    return M;
 } 
 
-void printMatrix(int** matrix, int n_rows, int n_cols) {
-    for(int i = 0; i < n_rows; ++i) {
+
+void printMatrix(Matrix matrix) {
+    for(int i = 0; i < matrix.rows; ++i) {
         printf("[");
-        for(int j = 0; j < n_cols; ++j) {
-            if (j != n_cols-1) printf("%5d, ", matrix[i][j]);
-            else printf("%5d", matrix[i][j]);
+        for(int j = 0; j < matrix.cols; ++j) {
+            if (j != matrix.cols-1) printf("%5d, ", matrix.values[i][j]);
+            else printf("%5d", matrix.values[i][j]);
             
             
         }
@@ -63,21 +72,27 @@ int main() {
         A[i] = calloc(a_cols, sizeof(int));
         for(int j = 0; j < a_cols; ++j) A[i][j] = a[i][j];
     }
+    Matrix M1 = {.values = A, .rows = a_rows, .cols = a_cols};
+
     printf("Matrix A:\n");
-    printMatrix(A, a_rows, a_cols);
+    printMatrix(M1);
 
     int** B = calloc(b_rows, sizeof(int*));
     for(int i = 0; i < b_rows; ++i) {
         B[i] = calloc(b_cols, sizeof(int));
         for(int j = 0; j < b_cols; ++j) B[i][j] = b[i][j];
     }
+    Matrix M2 = {.values = B, .rows = b_rows, .cols = b_cols};
     printf("Matrix B:\n");
-    printMatrix(B, b_rows, b_cols);
+    printMatrix(M2);
 
-    int** C;
     printf("Matrix C = AB:\n");
-    C = classic(A, B, a_rows, a_cols, b_cols);
-    printMatrix(C, c_rows, c_cols);
+    Matrix C = classic(M1, M2);
+    printMatrix(C);
+
+    free(M1.values);
+    free(M2.values);
+    free(C.values);
 
     return 0;
 }
